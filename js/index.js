ae22030da8c341b2730d2093d836e0a196fda0ab
Santiago Hernandez <santuche90@gmail.com>
2023-09-18 22:04:38 -0300

    Button Answer
    the button no prompts a confirmation

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("hero-btn");
+
+    btn.addEventListener("click", function () {
+        alert("moio te aaaamo")
+    })
+})
